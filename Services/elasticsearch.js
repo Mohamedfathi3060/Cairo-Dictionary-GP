7c6353f9cd9b5e7be5a0c7b80fbf4b6e9520b6f7
Mohamed Fathi <Mohamedfathi11119@gmail.com>
2025-06-25 23:26:42 +0300

    fix bug on elastic

--- Services/elasticsearch.js
+++ Services/elasticsearch.js
@@ -1,10 +1,14 @@
 const { Client } = require('@elastic/elasticsearch');
-
-const client = new Client({
-  node: process.env.ELASTIC_URL,
-  auth: {
-    apiKey: process.env.ELASTIC_API_KEY,
-  },
-});
+let client;
+try {
+  client = new Client({
+    node: process.env.ELASTIC_URL,
+    auth: {
+      apiKey: process.env.ELASTIC_API_KEY,
+    },
+  });
+} catch (e) {
+  console.log(e);
+}
 
 module.exports = client;
